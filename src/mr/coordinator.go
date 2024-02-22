b90854d64af7ccd110f64d342838e9c72cc408bd
Wenlong Dong <wenldong666@gmail.com>
2024-02-22 22:23:25 +0800

    [init]: code init and hello rpc impl
    Signed-off-by: Wenlong Dong <wenldong666@gmail.com>

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,38 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
+type Task struct {
+	name string
+}
 
 type Coordinator struct {
 	// Your definitions here.
-
+	task Task
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+func (c *Coordinator) GetTask(args *TaskRequest, reply *TaskResponse) error {
+	reply.Name = c.task.name
+	return nil
+}
 
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +46,27 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		task: Task{
+			name: "hello",
+		},
+	}
 
 	// Your code here.
-
 
 	c.server()
 	return &c
